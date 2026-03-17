39cf7f7d2bc74379b4aee23ebd7ed94134a1a753
Tarekul <tarekul03.muslim@gmail.com>
2026-03-17 15:39:33 +0530

    changes base_url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const BASE_URL = 'http://localhost:8000/api/v1';
+const BASE_URL = 'https://medix-pos-backend.onrender.com/api/v1';
 
 const api = axios.create({
   baseURL: BASE_URL,
